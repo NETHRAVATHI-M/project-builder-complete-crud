13e6b54831e6d0e33aa5b91926c1c3d15b0f1e65
NETHRAVATHI-M <nethravathim72@gmail.com>
2020-08-23 16:00:26 +0530

    done

--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,5 +1,37 @@
 package utility;
 
-public class ConnectionManager {
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
 
+public class ConnectionManager {
+	
+	public static Connection getConnection()  {
+		Connection con = null;
+		try {
+		Properties prop  = loadPropertiesFile() ;
+		String driver = prop.getProperty("driver");
+		Class.forName(driver);
+	 con =	DriverManager.getConnection(prop.getProperty("url"),prop.getProperty("username"),prop.getProperty("password"));
+	
+		}catch (Exception e) {
+				System.out.println(e.getMessage());
+		}
+		return con;
+		
+		
+	}
+
+	private static Properties loadPropertiesFile() throws IOException {
+		// TODO Auto-generated method stub
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+		
+	}
+	
 }
